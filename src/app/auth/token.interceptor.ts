3a29cd3609c4b0b19b14adfa33db7f56d82358f0
Istvan <istvan_gyarmati@epam.com>
2021-10-12 18:32:22 +0200

    -

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { SessionStorageService } from './services/session-storage/session-storage.service';
+import { catchError, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(public sessionStorageService: SessionStorageService) {}
+  constructor(public sessionStorageService: SessionStorageService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let token = this.sessionStorageService.getToken('bearerToken');
@@ -16,6 +18,14 @@ export class TokenInterceptor implements HttpInterceptor {
         },
       });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          this.sessionStorageService.deleteToken('bearerToken');
+          this.router.navigate(['/login']);
+        }
+        return of(err);
+      })
+    );
   }
 }
